a1d6658b5f2e0973e5a2594015073a1b12155c63
rustamkremcheev <rustam.omg1996@gmail.com>
2022-11-18 15:58:02 +0500

    I'm finished.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+
+    public final double number = 5.0;
+
     void divide(int toBeDivided) {
+       double result = toBeDivided / number;
+       System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+
+    public final float secondCount = 60.0F;
+
     public void convert(float minutes) {
+        float count = minutes * secondCount;
+        System.out.println(count);
     }
 }
